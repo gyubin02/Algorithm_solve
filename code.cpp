5cab1dc57d628554f6d87ca462158aad72ce372e
gyubin02 <dlrbqls980@gmail.com>
2023-12-25 23:38:43 +0900

    문제 ID : BOARDCOVER

--- code.cpp
+++ code.cpp
@@ -1,78 +1,74 @@
-//문제 ID : PICNIC
+//문제 ID : BOARDCOVER
 #include <iostream>
+#include <vector>
 using namespace std;
-int func(bool areFriends[10][10], bool taken[10], int n);
-int func_wrong(bool areFriends[10][10], bool taken[10], int n);
+
+bool set(vector<vector<int>>& board, int delta, int type, int x, int y);
+int func(vector<vector<int>>& board);
+
+
+int blockType[4][3][2] = {
+	{{0,0},{0,1},{1,0}},
+	{{0,0},{0,1},{1,1}},
+	{{0,0},{1,0},{1,1}},
+	{{0,0},{1,0},{1,-1}}
+};
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int cases;
-
+	int cases = 0;
 	cin >> cases;
 	while (cases-- > 0) {
-		//초기화
-		int n = 0; //n 초기화
-		int pairs = 0; //pairs 초기화
-		int arr[90] = { 0 }; //arr 초기화, pairs의 최대값이 45이므로 arr은 90칸입니다.
-		bool areFreinds[10][10] = { false };
-		bool taken[10] = { false };
 		//
-		cin >> n >> pairs;
-		for (int i = 0; i < 2 * pairs; i++) cin >> arr[i];
-		for (int i = 0; i < pairs; i++) {
-			int x = i * 2;
-			int hanni = arr[x];
-			int minji = arr[x + 1];
-			areFreinds[hanni][minji] = areFreinds[minji][hanni] = true;
+		int h = 0;
+		int w = 0;
+		//
+		cin >> h >> w;
+		vector<vector<int>> board(h);
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				char c;
+				cin >> c;
+				int a;
+				if (c == '#') a = 1;
+				else if (c == '.') a = 0;
+				board[i].push_back(a);
+			}
 		}
-		cout << func(areFreinds, taken, n) << "\n";
+			
+		cout << func(board) << "\n";
 	}
 	return 0;
 }
 
-int func(bool areFriends[10][10], bool taken[10], int n) {
-	//base case
-	int first = -1;
-	for (int i = 0; i < n; i++) {
-		if (!taken[i]) {
-			first = i;
-			break;
-		}
+bool set(vector<vector<int>>& board, int delta, int type, int x, int y) {
+	bool possible = true;
+	for (int i = 0; i < 3; i++) {
+		int nx = x + blockType[type][i][1];
+		int ny = y + blockType[type][i][0];
+		if (nx < 0 || nx >= board[0].size() || ny < 0 || ny >= board.size()) possible = false;
+		else if ((board[ny][nx] += delta) > 1) possible = false;
 	}
-	if (first == -1) return 1;
-
-	//step
-	int sum = 0;
-	for (int i = first + 1; i < n; i++) {
-		if (!taken[i] && areFriends[i][first]) {
-			taken[first] = taken[i] = true;
-			sum += func(areFriends, taken, n);
-			taken[first] = taken[i] = false;
-		}
-	}
-	return sum;
+	return possible;
 }
 
-int func_wrong(bool areFriends[10][10], bool taken[10], int n) {
-	//base case
-	bool finished = true;
-	for (int i = 0; i < n; i++) {
-		if (!taken[i]) finished = false;
-	}
-	if (finished) return 1;
-
-	//step
-	int sum = 0;
-	for (int i = 0; i < n; i++) { //다른 순서로 짝지어 주는 경우가 중복됩니다.
-		for (int j = i + 1; j < n; j++) {
-			if (!taken[i] && !taken[j] && areFriends[i][j]) {
-				taken[i] = taken[j] = true;
-				sum += func(areFriends, taken, n);
-				taken[i] = taken[j] = false;
-			}
+int func(vector<vector<int>>& board) {
+	int firstX = -1;
+	int firstY = -1;
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[i].size(); j++) {
+			if (board[i][j] == 0) { firstX = j; firstY = i; break; }
 		}
+		if (firstX != -1) break;
+	}
+	if (firstX == -1) return 1;
+	int sum = 0;
+	for (int type = 0; type < 4; type++) {
+		if (set(board, 1, type, firstX, firstY))
+			sum += func(board);
+		set(board, -1, type, firstX, firstY);
 	}
 	return sum;
 }
