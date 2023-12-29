d229b245e34eb9cc1c23ff670f56349974d6506f
gyubin02 <dlrbqls980@gmail.com>
2023-12-29 22:01:14 +0900

    문제 ID : WILDCARD (미완성 : O(n^2) 알고리즘 구현 가능)

--- wildcard.cpp
+++ wildcard.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 bool match(string::iterator& it, string::iterator& it2);
 bool match2(const string& s1, const string& s2);
+int match3(int x, int y);
 
 string s1, s2;
+int cache[101][101];
 
 
 int main() {
@@ -16,18 +18,26 @@ int main() {
 	int cases = 0;
 	cin >> cases;
 	while (cases-- > 0) {
+
 		cin >> s1;
 		int n;
 		cin >> n;
 		vector<string> ans;
 		while (n-- > 0) {
+			//悼利 拌裙过
+			for (int i = 0; i < 101; i++)
+				for (int j = 0; j < 101; j++)
+					cache[i][j] = -1;
+
 			cin >> s2;
 			/*
 			string::iterator it = s1.begin();
 			string::iterator it2 = s2.begin();
 			if (match(it, it2)) cout << s2 << "\n";
 			*/
-			if (match2(s1, s2)) ans.push_back(s2);
+			/*
+			if (match2(s1, s2)) ans.push_back(s2);*/
+			if (match3(0, 0)) ans.push_back(s2);
 		}
 		sort(ans.begin(), ans.end());
 		for (int i = 0; i < ans.size(); i++) cout << ans[i] << "\n";
@@ -60,4 +70,17 @@ bool match2(const string& s1, const string& s2) {
 		for (int skip = 0; pos + skip <= s2.size(); skip++)
 			if (match2(s1.substr(pos + 1), s2.substr(pos + skip))) return true;
 	return false;
+}
+
+//悼利 拌裙过 O(n^3)
+int match3(int x, int y) {
+	int& ret = cache[x][y];
+	if (ret != -1) return ret;
+	while (x < s1.size() && y < s2.size() && (s1[x] == s2[y] || s1[x] == '?')) { x++; y++; }
+	if (x == s1.size())
+		return ret = (y == s2.size());
+	if (s1[x] == '*')
+		for (int skip = 0; y + skip <= s2.size(); skip++)
+			if(match3(x+1, y+skip)) return ret = 1;
+	return ret = 0;
 }
